68ad0c6a7f80aba7d97f50af76c48498545c9d8f
Anuj3553 <anujverma3553@gmail.com>
2025-02-20 23:10:24 +0530

    feat: Basic Setup of Google login to fetch the user info with firebase popup and Implement Google login functionality and update Firebase configuration

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,7 +1,29 @@
+import Image from "next/image";
 import React from "react";
+import { FcGoogle } from "react-icons/fc";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth"
+import { firebaseAuth } from "@/utils/FirebaseConfig";
 
 function login() {
-  return <div>login</div>;
+  const handleLogin = async () => {
+    const provider = new GoogleAuthProvider();
+    const {
+      user: {displayName:name, email, photoURL: profileImage},
+    } = await signInWithPopup(firebaseAuth, provider);
+    console.log(user);
+  }
+  return (
+    <div className="flex flex-col justify-center items-center bg-panel-header-background h-screen w-screen">
+      <div className="flex items-center justify-center gap-2 text-white">
+        <Image src="/whatsapp.gif" alt="Whatsapp" height={300} width={300} />
+        <span className="text-7xl">Whatsapp</span>
+      </div>
+      <button className="flex items-center gap-7 bg-search-input-container-background p-5 rounded-lg mt-4" onClick={handleLogin}>
+        <FcGoogle className="text-4xl" />
+        <span className="text-white text-2xl">Login with Google</span>
+      </button>
+    </div>
+  );
 }
 
 export default login;
--- client/src/utils/FirebaseConfig.js
+++ client/src/utils/FirebaseConfig.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+// import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,6 +16,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(firebaseAuth);
 
-export default app;
+export const firebaseAuth = getAuth(app);
