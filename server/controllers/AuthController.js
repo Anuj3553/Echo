313b8715cbd63a306a6816a066ef9e2da68898f6
Anuj3553 <anujverma3553@gmail.com>
2025-02-22 16:00:50 +0530

    feat: Add getAllUsers endpoint to retrieve and group users by initial letter; enhance error handling in AuthController

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -23,9 +23,9 @@ export const checkUser = async (req, res, next) => {
             return res.json({ msg: "User found", status: true, data: user }) // Return a success response
         }
     } catch (err) {
-        next(err);
+        next(err); // Pass the error to the error handler
     }
-}
+};
 
 export const onBoardUser = async (req, res, next) => {
     try {
@@ -48,6 +48,39 @@ export const onBoardUser = async (req, res, next) => {
 
         return res.json({ msg: "Success", status: true, user }) // Return a success response
     } catch (err) {
-        next(err);
+        next(err); // Pass the error to the error handler
+    }
+};
+
+export const getAllUsers = async (req, res, next) => {
+    try {
+        const prisma = getPrismaInstance(); // Get the PrismaClient instance
+        const users = await prisma.user.findMany({
+            orderBy: { // Order the users by name in ascending order
+                name: 'asc' // Order the users by name in ascending order
+            },
+            select: { // Select the following fields
+                id: true, // Select the id field
+                email: true, // Select the email field
+                name: true, // Select the name field
+                profilePicture: true, // Select the profilePicture field
+                about: true, // Select the about field
+            }
+        });
+
+        // Create an empty object to store the users grouped by the initial letter of their name
+        const userGroupByInitialLetter = {};
+        // Loop through the users
+        users.forEach(user => {
+            const initialLetter = user.name.charAt(0).toUpperCase(); // Get the first letter of the user's name
+            if (!userGroupByInitialLetter[initialLetter]) { // If the initial letter does not exist in the object
+                userGroupByInitialLetter[initialLetter] = []; // Create an empty array
+            }
+            userGroupByInitialLetter[initialLetter].push(user); // Push the user to the array
+        });
+
+        return res.status(200).send({ users: userGroupByInitialLetter }); // Return a success response
+    } catch (err) {
+        next(err); // Pass the error to the error handler
     }
 }
